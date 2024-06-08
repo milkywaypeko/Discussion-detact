4a4017543c30d574a5f8d04d2a37a9a396615f1f
Choi Hyeongjong <milkywaypeko@seoultech.ac.kr>
2024-06-09 02:28:55 +0900

    npm 적용버전

---
+++ discussion-detact/src/background.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// With background scripts you can communicate with popup
+// and contentScript files.
+// For more information on background script,
+// See https://developer.chrome.com/extensions/background_pages
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.local.set({enabled: true}); // 기본값 설정
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'answers') {
+        console.log("Received answers:", message.data);
+        let result = [];
+        
+        const fetchPromises = message.data.map(SerTe => {
+            console.log(SerTe.text);
+            return fetch('http://selogic.seoultech.ac.kr:8000/process', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({sentence: SerTe.text}),
+                credentials: "include",
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                result.push({issueAnchor : SerTe.issueAnchor , textType : data});
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                result.push({issueAnchor : SerTe.issueAnchor , textType : error});
+            });
+        });
+
+        Promise.all(fetchPromises).then(() => {
+            console.log(result);
+            sendResponse(result);
+        });
+
+        return true;
+    }
+});
---
+++ discussion-detact/src/contentScript.js
@@ -0,0 +1,94 @@
+function extractAnswers() {
+    const answers = [];
+    // Github의 답변들은 일반적으로 특정 클래스를 가진 요소 내에 위치
+    // 주의: Github의 html 구조가 변경될 수 있으므로, 필요에 따라 셀렉터를 업데이트
+    //js-timeline-progressive-focus-container
+    const answerElements = document.querySelectorAll(".js-timeline-item");
+    var count = 0;
+    answerElements.forEach(elem => {
+        // 답변 내용을 추출합니다. 내용이 길 경우 더 많은 처리 필요
+        const answerBody = elem.querySelectorAll(".js-comment-body");
+        answerBodytext = answerBody.forEach(texts => {
+            const answerstext = texts.querySelectorAll("p");
+            answerST = answerstext.forEach(STs => {
+                const answerSText = {
+                    issueAnchor : 'IssueText' + count,
+                    text : STs.innerText,
+                };
+                answers.push(answerSText);
+                STs.setAttribute('class', 'IssueText' + count);
+                count += 1;
+            }); 
+        });
+    });
+    return answers;
+}
+
+// 답변 데이터를 추출하고, console.log를 사용하여 출력
+// 실제로는 이 데이터를 서버에서 처리
+const answers = extractAnswers();
+console.log(answers);
+
+classFy_answers = [];
+
+// background.js로 데이터를 보낼 때 사용
+chrome.runtime.sendMessage({type : 'answers', data : answers}, (response) => {
+    // 3. Got an asynchronous response with the data from the service worker
+    console.log('received user data', response);
+
+    //그냥 이안에서 response 사용하시면 됩니다.
+    for(let i = 0; i < response.length; i++) {
+        if (response[i].textType.result === "Action on Issue")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'lightcoral';
+        } 
+        else if (response[i].textType.result === "Bug Reproduction")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'lime';
+        }
+        else if (response[i].textType.result === "Contribution and Commitment")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'olive';
+        }
+        else if (response[i].textType.result === "Expected Behaviour")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'coral';
+        }
+        else if (response[i].textType.result === "Investigation and Exploration")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'aliceblue';
+        }
+        else if (response[i].textType.result === "Motivation")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'aquamarine';
+        }
+        else if (response[i].textType.result === "Observed Bug Behaviour")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'burlywood';
+        }
+        else if (response[i].textType.result === "Potential New Issues and Requests")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'greenyellow';
+        }
+        else if (response[i].textType.result === "Social Conversation")
+        {   
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'cyan';
+        }
+        else if (response[i].textType.result === "Solution Discussion")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'plum';
+        }
+        else if (response[i].textType.result === "Solution Usage")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'aqua';
+        }
+        else if (response[i].textType.result === "Task Progress")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'azure';
+        }
+        else if (response[i].textType.result === "Workarounds")
+        {
+            document.querySelector('.' + response[i].issueAnchor).style.backgroundColor = 'beige';
+        };
+    };
+});
